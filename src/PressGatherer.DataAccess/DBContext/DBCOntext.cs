c82b94953661276cd637bf4822632cf1649df4f3
Sceurpien <sceurpien@gmail.com>
2018-12-15 00:57:37 +0100

    DBContext

---
+++ src/PressGatherer.DataAccess/DBContext/DBCOntext.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using PressGatherer.DataAccess;
+
+namespace PressGatherer.DataAccess
+{
+    public class DbContext
+    {
+        public const string DATABASE_NAME = "pg";
+        public const string USERS_COLLECTION_NAME = "pgusers";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _database;
+
+        static DbContext()
+        {
+            var connectionString = "mongodb://localhost:27017";
+            _client = new MongoClient(connectionString);
+            _database = _client.GetDatabase(DATABASE_NAME);
+        }
+
+        public IMongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoCollection<PGUser> Users
+        {
+            get { return _database.GetCollection<PGUser>(USERS_COLLECTION_NAME); }
+        }
+    }
+}
---
+++ src/PressGatherer.DataAccess/DataAccessLayer/GetUser.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using PressGatherer.DataAccess;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressGatherer.DataAccess.DataAccessLayer
+{
+    public partial class PGAccess
+    {
+        public static async Task<PGUser> GetUser (string id)
+        {
+            PGUser returnvalue = new PGUser();
+
+            DbContext db = new DbContext();
+            var users = await db.Users.Find(x => x.Id == new ObjectId(id))
+                                        .Project<PGUser>("{name : 1, type : 1}")
+                                        .ToListAsync();
+            returnvalue = users.Single();
+
+            return returnvalue;
+        }
+    }
+}
