4420bb52a61f64b5f8a6fd1e80dc391b1a2b41f9
Sceurpien <sceurpien@gmail.com>
2018-12-16 04:20:31 +0100

    Create Page

---
+++ src/PressGatherer.BusinessLogic/Logic/ArticleModules/CreatePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using PressGatherer.DataAccess.DataAccessLayer;
+using PressGatherer.References.TransportModels.ArticleModules;
+
+namespace PressGatherer.BusinessLogic.ArticleModules
+{
+    public partial class ArticleDriver
+    {
+        public static async Task<CreatePageTransportResponseModel> CreatePage(CreatePageTransportRequestModel request)
+        {
+            CreatePageTransportResponseModel response = new CreatePageTransportResponseModel();
+
+            response = await PGAccess.CreatePage(request);
+
+            return response;
+        }
+    }
+}
---
+++ src/PressGatherer.DataAccess/DataAccessLayer/ArticleModules/CreatePage.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using PressGatherer.References.Enums;
+using PressGatherer.References.Logics;
+using PressGatherer.References.TransportModels.ArticleModules;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PressGatherer.DataAccess.DataAccessLayer
+{
+    public partial class PGAccess
+    {
+        public static async Task<CreatePageTransportResponseModel> CreatePage (CreatePageTransportRequestModel model)
+        {
+            try
+            {                
+                DbContext db = new DbContext();
+
+                Page page = new Page
+                {
+                    AddedDate = DateTime.UtcNow,
+                    BaseLink = model.BaseLink,
+                    Name = model.Name,
+                    RssLink = model.RssLink,
+                    SearchLink = model.SearchLink
+                };
+
+                await db.Pages.InsertOneAsync(page);
+
+                return new CreatePageTransportResponseModel(page.Id.ToString());
+            }
+            catch
+            {
+                return new CreatePageTransportResponseModel();
+            }
+        }
+    }
+}
