e73d2ba846de6c440c5d572b8177ac90da70e363
csibivili <csibi.vili@gmail.com>
2019-01-03 16:48:50 +0100

    Add User To Search Group

---
+++ src/PressGatherer.DataAccess/DataAccessLayer/SearchModules/AddUserToSearchGroup.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using PressGatherer.References.Enums;
+using PressGatherer.References.TransportModels.SearchModules;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PressGatherer.DataAccess.DataAccessLayer
+{
+    public partial class PGAccess
+    {
+        public static async Task<AddUserToSearchGroupTransportResponseModel> AddUserToSearchGroup(AddUserToSearchGroupTransportRequestModel model)
+        {
+            try
+            {
+                DbContext db = new DbContext();
+
+                var group = await db.SearchGroups.Find(x => x.GroupName == model.GroupName).SingleAsync();
+
+                if (!group.Users.Any(x => x.UserId == model.UserId))
+                {
+                    var user = new SearchGroupAccess
+                    {
+                        UserId = model.UserId,
+                        AccessType = UserAccessTypeEnum.View
+                    };
+
+                    group.Users.Append(user);
+                    group.LastChangedDate = DateTime.UtcNow;
+
+                    await db.SearchGroups.ReplaceOneAsync(x => x.GroupName == model.GroupName, group);
+                }
+
+                return new AddUserToSearchGroupTransportResponseModel(true); //still true if already exists?
+            }
+            catch
+            {
+                return new AddUserToSearchGroupTransportResponseModel();
+            }
+        }
+    }
+}
---
+++ src/PressGatherer.References/TransportModels/SearchModules/RequestModels/AddUserToSearchGroupTransportRequestModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PressGatherer.References.TransportModels.SearchModules
+{
+    public class AddUserToSearchGroupTransportRequestModel
+    {
+        public string GroupName { get; set; }
+        public string UserId { get; set; }
+
+        public AddUserToSearchGroupTransportRequestModel()
+        {
+            GroupName = "";
+            UserId = "";
+        }
+
+        public AddUserToSearchGroupTransportRequestModel(string groupName, string userId)
+        {
+            GroupName = groupName;
+            UserId = userId;
+        }
+    }
+}
