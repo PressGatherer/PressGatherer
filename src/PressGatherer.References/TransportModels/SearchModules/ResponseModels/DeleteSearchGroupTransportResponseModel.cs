f5fdf38e42c90ba6edf9704ed4454170d5d8f70f
blakii <attila.kardos1981@gmail.com>
2019-02-17 19:58:33 +0100

    Delete search group

---
+++ src/PressGatherer.References/TransportModels/SearchModules/ResponseModels/DeleteSearchGroupTransportResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PressGatherer.References.TransportModels.SearchModules
+{
+    public class DeleteSearchGroupTransportResponseModel
+    {
+        public bool IsSuccesful { get; set; }
+
+        public DeleteSearchGroupTransportResponseModel(bool isSuccesful)
+        {
+            this.IsSuccesful = isSuccesful;
+        }
+    }
+}
