39bda2cfc9f7310f70ea0af744c7bec92083c314
blakii <attila.kardos1981@gmail.com>
2019-02-17 20:37:38 +0100

    Implemented missing logic to successful tests.

--- src/PressGatherer.DataAccess/DataAccessLayer/SearchModules/AddSearchWordToSearchGroup.cs
+++ src/PressGatherer.DataAccess/DataAccessLayer/SearchModules/AddSearchWordToSearchGroup.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using PressGatherer.References.Exceptions;
 using PressGatherer.References.TransportModels.SearchModules;
 
 namespace PressGatherer.DataAccess.DataAccessLayer
@@ -11,13 +12,31 @@ namespace PressGatherer.DataAccess.DataAccessLayer
     {
         public static async Task<bool> AddSearchWordToSearchGroup(AddSearchWordToSearchGroupTransportRequestModel model)
         {
+
+            if (string.IsNullOrWhiteSpace(model.SearchGroupId))
+            {
+                throw new MissingSearchGroupException();
+            }
+            if (string.IsNullOrWhiteSpace(model.Word))
+            {
+                throw new MissingWordException();
+            }
+
+            DbContext db = new DbContext();
+
+            SearchGroup group = (await db.SearchGroups.Find(x => x.Id == new ObjectId(model.SearchGroupId)).ToListAsync()).Single();
+
+            if (group.SearchWords.Any(x => x.Word == model.Word))
+            {
+                throw new DuplicateWordException();
+            }
+
             try
             {
-                DbContext db = new DbContext();
-
                 SearchWord wordToAdd = new SearchWord { Word = model.Word, Order = model.Order };
 
                 var filter = Builders<SearchGroup>.Filter.Eq(e => e.Id, new ObjectId(model.SearchGroupId));
+
                 var update = Builders<SearchGroup>.Update.Push<SearchWord>(e => e.SearchWords, wordToAdd);
                 await db.SearchGroups.FindOneAndUpdateAsync(filter, update);
 
