2ffed36067158798afca125ec0ba30377acc43ed
blakii <attila.kardos1981@gmail.com>
2019-02-25 09:22:02 +0100

    RefreshStatus update in SearchGroup

---
+++ src/PressGatherer.Test.BusinessLogic/UnitTests/SearchModules/RefreshStatusUpdateInSearchGroup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PressGatherer.BusinessLogic.SearchModules;
+using PressGatherer.DataAccess.DataAccessLayer;
+using PressGatherer.References.TransportModels.SearchModules;
+using PressGatherer.References.Exceptions;
+using System;
+using System.Threading.Tasks;
+
+namespace PressGatherer.Test.BusinessLogic
+{
+    [TestClass]
+    public class RefreshStatusUpdateInSearchGroup
+    {
+        [TestMethod]
+        public async Task RefreshStatudUpdate_Successful()
+        {
+            string searchGroupId = await GetSearchGroupId();
+
+            var model = new RefreshStatusUpdateInSearchGroupTransportRequestModel(searchGroupId, 9);
+            var result = await SearchDriver.RefreshStatusUpdateInSearchGroup(model);
+            Assert.IsTrue(result.IsSuccesful);
+        }
+
+
+        [TestMethod]
+        public async Task RefreshStatudUpdate_MissingSearchGroupId()
+        {
+            try
+            {
+                var model = new RefreshStatusUpdateInSearchGroupTransportRequestModel("", 9);
+                var result = await SearchDriver.RefreshStatusUpdateInSearchGroup(model);
+                Assert.Fail();
+            }
+            catch (MissingSearchGroupException) { }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        public async Task<string> GetSearchGroupId()
+        {
+            var userid = PGAccessForTest.GetFirstUserId();
+            var model = new CreateSearchGroupTransportRequestModel("Test_" + DateTime.UtcNow.ToString(), userid.Result);
+            var result = await PGAccess.CreateSearchGroup(model);
+
+            return result.SearchGroupId;
+        }
+    }
+}
